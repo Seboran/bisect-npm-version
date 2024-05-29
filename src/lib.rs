27e2eb01326323b31aaeaacb025266f532cd6caf
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-05-29 09:55:54 +0200

    refactor: removes evil clone

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ pub fn find_not_working_version(
     working_version: String,
     not_working_version: String,
     script_to_run: fn(current_version: &String) -> bool,
-    list_of_versions: Vec<String>,
-) -> Option<String> {
+    list_of_versions: &Vec<String>,
+) -> Option<&String> {
     let index_working_version = list_of_versions.binary_search(&working_version);
     let index_not_working_version = list_of_versions.binary_search(&not_working_version);
     match (index_working_version, index_not_working_version) {
@@ -11,11 +11,11 @@ pub fn find_not_working_version(
             let list_versions_to_test =
                 &list_of_versions[index_working_version - 1..index_not_working_version];
 
-            let mut result_version: Option<String> = None;
+            let mut result_version: Option<&String> = None;
 
             for version in list_versions_to_test {
                 if script_to_run(version) {
-                    result_version = Some(version.clone());
+                    result_version = Some(version);
                 }
             }
             result_version
@@ -42,18 +42,19 @@ mod tests {
                 _ => true,
             }
         }
+        let versions = vec![
+            String::from("3.0.1"),
+            String::from("4.0.8"),
+            String::from("4.5.0"),
+            String::from("5.0.0"),
+            String::from("5.4.0"),
+        ];
         let not_working_version = find_not_working_version(
             String::from("4.0.8"),
             String::from("5.4.0"),
             test_version,
-            vec![
-                String::from("3.0.1"),
-                String::from("4.0.8"),
-                String::from("4.5.0"),
-                String::from("5.0.0"),
-                String::from("5.4.0"),
-            ],
+            &versions,
         );
-        assert_eq!(not_working_version, Some(String::from("5.0.0")))
+        assert_eq!(not_working_version, Some(&String::from("5.0.0")))
     }
 }
