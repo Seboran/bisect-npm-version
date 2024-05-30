5ebd9390a4d421f2a92a9fd86672a0be8f9467e8
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-05-30 22:18:04 +0200

    feat: permet de trouver la version non fonctionnel d'une dépendance

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
-pub fn find_not_working_version(
-    working_version: String,
-    not_working_version: String,
-    script_to_run: fn(current_version: &String) -> bool,
-    list_of_versions: &Vec<String>,
-) -> Option<&String> {
-    let index_working_version = list_of_versions.binary_search(&working_version);
-    let index_not_working_version = list_of_versions.binary_search(&not_working_version);
+use std::{
+    path::PathBuf,
+    process::{self, Command},
+};
+
+pub fn find_not_working_version<'a>(
+    working_version: &String,
+    not_working_version: &String,
+    script_to_run: impl Fn(&String) -> bool,
+    list_of_versions: &'a Vec<String>,
+) -> Option<&'a String> {
+    let index_working_version = list_of_versions.iter().position(|v| v == working_version);
+    let index_not_working_version = list_of_versions
+        .iter()
+        .position(|v| v == not_working_version);
     match (index_working_version, index_not_working_version) {
-        (Ok(index_working_version), Ok(index_not_working_version)) => {
+        (Some(index_working_version), Some(index_not_working_version)) => {
             let list_versions_to_test =
-                &list_of_versions[index_working_version - 1..index_not_working_version];
+                &list_of_versions[index_working_version..index_not_working_version + 1];
 
             let partition_point =
-                list_of_versions.partition_point(|version| !script_to_run(version));
-            list_versions_to_test.get(partition_point)
+                list_versions_to_test.partition_point(|version| script_to_run(version));
+            list_versions_to_test.get(partition_point + 1)
         }
         (err1, err2) => {
             todo!(
@@ -23,6 +30,23 @@ pub fn find_not_working_version(
             )
         }
     }
+}
+
+pub fn is_ok_script(path_to_script: &PathBuf, package_name: &String, version: &String) -> bool {
+    println!("Trying {}@{}", package_name, version);
+    let install_result = Command::new("pnpm")
+        .arg("add")
+        .arg(format!("{package_name}@{version}"))
+        .output();
+
+    if let Err(install_error) = install_result {
+        println!("Command failed with {}", install_error);
+        process::exit(1);
+    }
+
+    let script_output = Command::new(path_to_script).output().unwrap();
+
+    script_output.status.success()
 }
 
 #[cfg(test)]
@@ -45,8 +69,8 @@ mod tests {
             String::from("5.4.0"),
         ];
         let not_working_version = find_not_working_version(
-            String::from("4.0.8"),
-            String::from("5.4.0"),
+            &String::from("4.0.8"),
+            &String::from("5.4.0"),
             test_version,
             &versions,
         );
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::{
+    path::PathBuf,
+    process::{self, Command},
+};
+
+use bisect_npm_version::{find_not_working_version, is_ok_script};
+
 fn main() {
-    println!("Hello, world!");
+    // Get package name, working version, current version from command line
+    let args: Vec<String> = std::env::args().collect();
+    let package_name = &args[1];
+    let not_working_version = &args[2];
+    let working_version = &args[3];
+    let script_path = &args[4];
+
+    let list_of_versions = Command::new("npm")
+        .args(["view", package_name, "versions", "--json"])
+        .output()
+        .expect("ntm")
+        .stdout;
+
+    let unparsed_list_of_versions = match String::from_utf8(list_of_versions) {
+        Ok(v) => v,
+        Err(_) => {
+            println!("Cannot get versions");
+            process::exit(1)
+        }
+    };
+
+    let list_of_versions = match serde_json::from_str::<Vec<String>>(&unparsed_list_of_versions) {
+        Ok(v) => v,
+        Err(e) => {
+            println!(
+                "There was an error parsing list of {package_name} versions, {}",
+                e
+            );
+            process::exit(1)
+        }
+    };
+    println!("Versions of {package_name}, {:?}", list_of_versions);
+
+    let default_value = String::from("rip is peace");
+    let find_not_working_version = find_not_working_version(
+        working_version,
+        not_working_version,
+        |version: &String| is_ok_script(&PathBuf::from(script_path), package_name, version),
+        &list_of_versions,
+    )
+    .unwrap_or(&default_value);
+
+    println!("Failing version: {find_not_working_version}");
 }
